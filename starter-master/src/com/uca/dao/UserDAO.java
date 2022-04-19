bf92e03072533087ed6ab352a2af1569186b1c9a
Alexis DRAI <drai.alexis@gmail.com>
2022-04-19 20:44:16 +0200

    Improve DB creation, remove 1st insert

--- starter-master/src/com/uca/dao/UserDAO.java
+++ starter-master/src/com/uca/dao/UserDAO.java
@@ -5,19 +5,25 @@ import com.uca.entity.UserEntity;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class UserDAO extends _Generic<UserEntity> {
-// CRUD Create Read (Update) Delete
+public class UserDAO extends _Generic<UserEntity>
+{
+    // CRUD Create Read (Update) Delete
 
-// TODO ? Read one? Read some?
+    // TODO ? Read one? Read some?
 
-// Read all
+    // Read all
 
-    public ArrayList<UserEntity> getAllUsers() {
+    public ArrayList<UserEntity> getAllUsers()
+    {
         ArrayList<UserEntity> entities = new ArrayList<>();
-        try {
-            PreparedStatement preparedStatement = this.connect.prepareStatement("SELECT * FROM Teacher ORDER BY id_teacher ASC;");
+        try
+        {
+            PreparedStatement preparedStatement = this.connect.prepareStatement(
+                    "SELECT * FROM Teacher ORDER BY id_teacher ASC;");
+
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
+            while (resultSet.next())
+            {
                 UserEntity entity = new UserEntity();
                 entity.setId(resultSet.getInt("id_teacher"));
                 entity.setFirstName(resultSet.getString("firstname"));
@@ -25,40 +31,48 @@ public class UserDAO extends _Generic<UserEntity> {
 
                 entities.add(entity);
             }
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             e.printStackTrace();
         }
 
         return entities;
     }
 
-    // TODO test this
+    // TODO redo with CM slides and security in mind
     // TODO decide whether to keep ID in the code or leave it in the database
+
     /**
      * creates a user entity in database
-     * 
+     *
      * @param obj instance of the basic UserEntity class
      */
     @Override
-    public UserEntity create(UserEntity obj) {
-        int id = obj.getId();
+    public UserEntity create(UserEntity obj)
+    {
+        int    id    = obj.getId();
         String fName = obj.getFirstName();
         String lName = obj.getLastName();
-        
-        String query = String.format("INSERT INTO users (id_teacher, lastname, firstname) (%d, %s, %s);", id, fName, lName);
 
-        try {
+        String query = String.format("INSERT INTO users (id_teacher, lastname, firstname) (%d, %s, %s);",
+                                     id,
+                                     fName,
+                                     lName);
+
+        try
+        {
             PreparedStatement preparedStatement = this.connect.prepareStatement(query);
             preparedStatement.executeQuery();
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             e.printStackTrace();
         }
         return obj;
     }
 
-
     @Override
-    public void delete(UserEntity obj) {
+    public void delete(UserEntity obj)
+    {
         //TODO !
     }
 }
