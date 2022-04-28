8228f93d6f1dad421dd97fea3566f649d22c62cf
Alexis DRAI <drai.alexis@gmail.com>
2022-04-28 13:56:42 +0200

    Fix #1

--- starter-master/src/com/uca/entity/StickerEntity.java
+++ starter-master/src/com/uca/entity/StickerEntity.java
@@ -2,4 +2,42 @@ package com.uca.entity;
 
 public class StickerEntity
 {
+    private int    id_sticker;
+    private Color  color;
+    private String description;
+
+    public StickerEntity()
+    {
+        // never used
+    }
+
+    int getId_sticker()
+    {
+        return id_sticker;
+    }
+
+    void setId_sticker(int id_sticker)
+    {
+        this.id_sticker = id_sticker;
+    }
+
+    Color getColor()
+    {
+        return color;
+    }
+
+    void setColor(Color color)
+    {
+        this.color = color;
+    }
+
+    String getDescription()
+    {
+        return description;
+    }
+
+    void setDescription(String description)
+    {
+        this.description = description;
+    }
 }
