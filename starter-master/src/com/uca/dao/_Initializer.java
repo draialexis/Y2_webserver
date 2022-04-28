a2c097100a29a1dead00e5e58856918f733183cb
Alexis DRAI <drai.alexis@gmail.com>
2022-04-28 13:51:23 +0200

    Debug init SQL

--- starter-master/src/com/uca/dao/_Initializer.java
+++ starter-master/src/com/uca/dao/_Initializer.java
@@ -12,21 +12,22 @@ public class _Initializer
         try
         {
             PreparedStatement statement;
-
-            //Init articles table
             statement = connection.prepareStatement(
+                    "DROP TABLE IF EXISTS Pupil CASCADE;" + // temporary
                     "CREATE TABLE IF NOT EXISTS Pupil(" +
                     "   id_pupil INT AUTO_INCREMENT," +
                     "   lastname VARCHAR(50) NOT NULL," +
                     "   firstname VARCHAR(50) NOT NULL," +
                     "   CONSTRAINT PK_Pupil PRIMARY KEY(id_pupil)" +
                     ");" +
+                    "DROP TABLE IF EXISTS Sticker CASCADE;" + // temporary
                     "CREATE TABLE IF NOT EXISTS Sticker(" +
                     "   id_sticker INT AUTO_INCREMENT," +
                     "   color VARCHAR(20) NOT NULL, /* Only within {GREEN, WHITE, RED}*/" +
                     "   description VARCHAR(20) NOT NULL, /* Only within {STAR, TRIANGLE, CIRCLE, SQUARE} (???)*/" +
                     "   CONSTRAINT PK_Sticker PRIMARY KEY(id_sticker)" +
                     ");" +
+                    "DROP TABLE IF EXISTS Teacher CASCADE;" + // temporary
                     "CREATE TABLE IF NOT EXISTS Teacher(" +
                     "   id_teacher INT AUTO_INCREMENT," +
                     "   lastname VARCHAR(50) NOT NULL," +
@@ -36,6 +37,7 @@ public class _Initializer
                     "   CONSTRAINT PK_Teacher PRIMARY KEY(id_teacher)," +
                     "   CONSTRAINT AK_Teacher UNIQUE(username)" +
                     ");" +
+                    "DROP TABLE IF EXISTS Award;" + // temporary
                     "CREATE TABLE IF NOT EXISTS Award(" +
                     "   id_teacher INT," +
                     "   id_sticker INT," +
@@ -43,9 +45,9 @@ public class _Initializer
                     "   attribution_date DATE NOT NULL," +
                     "   motive TEXT NOT NULL," +
                     "   CONSTRAINT PK_Award PRIMARY KEY(id_teacher, id_sticker, id_pupil)," +
-                    "   CONSTRAINT FK_Award_Teacher FOREIGN KEY(id_teacher) REFERENCES Teacher(id_teacher) ON UPDATE CASCADE," +
-                    "   CONSTRAINT FK_Award_Sticker FOREIGN KEY(id_sticker) REFERENCES Sticker(id_sticker) ON UPDATE CASCADE," +
-                    "   CONSTRAINT FK_Award_Pupil FOREIGN KEY(id_pupil) REFERENCES Pupil(id_pupil) ON UPDATE CASCADE" +
+                    "   CONSTRAINT FK_Award_Teacher FOREIGN KEY(id_teacher) REFERENCES Teacher(id_teacher) ON UPDATE CASCADE ON DELETE CASCADE," +
+                    "   CONSTRAINT FK_Award_Sticker FOREIGN KEY(id_sticker) REFERENCES Sticker(id_sticker) ON UPDATE CASCADE ON DELETE CASCADE," +
+                    "   CONSTRAINT FK_Award_Pupil FOREIGN KEY(id_pupil) REFERENCES Pupil(id_pupil) ON UPDATE CASCADE ON DELETE CASCADE" +
                     ");"
             );
 
