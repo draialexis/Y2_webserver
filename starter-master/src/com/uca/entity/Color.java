99e8d5dab4673243a452ec56080af3a5ce02d040
Alexis DRAI <drai.alexis@gmail.com>
2022-05-01 14:25:05 +0200

    Fix #40

--- starter-master/src/com/uca/entity/Color.java
+++ starter-master/src/com/uca/entity/Color.java
@@ -2,5 +2,29 @@ package com.uca.entity;
 
 public enum Color
 {
-    GREEN, WHITE, RED
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    CYAN,
+    BLUE,
+    MAGENTA,
+    PURPLE,
+    WHITE,
+    BLACK,
+    GRAY,
+    SILVER,
+    PINK,
+    MAROON,
+    BROWN,
+    BEIGE,
+    TAN,
+    PEACH,
+    LIME,
+    OLIVE,
+    TURQUOISE,
+    TEAL,
+    NAVY_BLUE,
+    INDIGO,
+    VIOLET
 }
