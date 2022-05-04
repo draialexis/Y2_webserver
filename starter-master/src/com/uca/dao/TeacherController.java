10ea56afc22ed59be3643732f14c553f88c39e50
Eyosias1 <eyosias16@gmail.com>
2022-05-04 16:47:11 +0200

    wrote comments to explain the functionnality a little.

--- starter-master/src/com/uca/dao/TeacherController.java
+++ starter-master/src/com/uca/dao/TeacherController.java
@@ -2,19 +2,18 @@ package com.uca.dao;
 
 import com.uca.entity.TeacherEntity;
 import com.uca.core.TeacherCore;
-import com.uca.dao.PassWordBasedEncryption;
 
 public class TeacherController {
 
     public static boolean authenticate(String username, String password) {
-        if (username.isEmpty() || password.isEmpty()) {
+        if (username.isEmpty() || password.isEmpty()) { // tried to log in without putting in the password
             return false;
         }
         TeacherEntity user = TeacherCore.readByUserName(username);
-        if (user == null) {
+        if (user == null) { // means the user doesn't exist
             return false;
         }
-        return PassWordBasedEncryption.verifyUserPassword(password, user.getUserSPwd(), user.getUserHPwd());
+        return PassWordBasedEncryption.verifyUserPassword(password, user.getUserHPwd(), user.getUserSPwd()); // verifies if the password
     }
 
 
