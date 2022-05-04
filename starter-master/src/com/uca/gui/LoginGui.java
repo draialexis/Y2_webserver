c3994e9647915aea8e464ae7aff62788b8671281
Eyosias1 <eyosias16@gmail.com>
2022-05-04 16:37:26 +0200

    Created the LoginGui class.

--- starter-master/src/com/uca/gui/LoginGui.java
+++ starter-master/src/com/uca/gui/LoginGui.java
@@ -1,29 +1,28 @@
 package com.uca.gui;
 
-import freemarker.template.Configuration;
+
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
 
 
-public class LoginGui {
-
-    public static String LoginFailedGUI() throws TemplateException, IOException {
-
-        Configuration configuration = _FreeMarkerInitializer.getContext();
-        Map<String, Object> input = new HashMap<>();
-        Writer   output   = new StringWriter();
-        Template template = configuration.getTemplate("stickers/sticker.ftl");
-        template.setOutputEncoding("UTF-8");
-        template.process(input, output);
-
-        return output.toString();
+public class LoginGui extends _GenericGUI{
+    public static String LoginPageGUI() throws TemplateException, IOException {
+        Map<String, Object> input  = new HashMap<>();
+        input.put("login message", "Welcome to The login page");
+        Template template = configuration.getTemplate("Login/login.ftl");
+        return inAndOut(template, input, new StringWriter());
     }
 
+    public static String LoginFailedGUI() throws TemplateException, IOException {
+        Map<String, Object> input  = new HashMap<>();
+        input.put("login Failed", "wrong username or password");
+        Template template = configuration.getTemplate("Login/LoginFailed.ftl");
+        return inAndOut(template, input, new StringWriter());
+    }
 }
