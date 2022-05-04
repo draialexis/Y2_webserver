4ac2cff574f0afe33844a1483e86ce89389b5a01
Alexis DRAI <drai.alexis@gmail.com>
2022-05-04 23:50:43 +0200

    Fix #6, Fix #2

--- starter-master/src/com/uca/entity/AwardEntity.java
+++ starter-master/src/com/uca/entity/AwardEntity.java
@@ -1,5 +1,79 @@
 package com.uca.entity;
 
+import java.util.Date;
+// TODO use java.sql.Date instead?
+
 public class AwardEntity
 {
+    private TeacherEntity teacher;
+    private StickerEntity sticker;
+    private PupilEntity   pupil;
+    private Date          attributionDate;
+    private String        motive;
+
+    TeacherEntity getTeacher()
+    {
+        // TODO return a copy instead
+        return teacher;
+    }
+
+    void setTeacher(TeacherEntity teacher)
+    {
+        this.teacher = teacher;
+    }
+
+    StickerEntity getSticker()
+    {
+        // TODO return a copy instead
+        return sticker;
+    }
+
+    void setSticker(StickerEntity sticker)
+    {
+        this.sticker = sticker;
+    }
+
+    PupilEntity getPupil()
+    {
+        // TODO return a copy instead
+        return pupil;
+    }
+
+    void setPupil(PupilEntity pupil)
+    {
+        this.pupil = pupil;
+    }
+
+    Date getAttributionDate()
+    {
+        // TODO return a copy instead ?
+        return attributionDate;
+    }
+
+    void setAttributionDate(Date attributionDate)
+    {
+        this.attributionDate = attributionDate;
+    }
+
+    String getMotive()
+    {
+        return motive;
+    }
+
+    void setMotive(String motive)
+    {
+        this.motive = motive;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "AwardEntity{" +
+               "teacher=" + teacher +
+               ", sticker=" + sticker +
+               ", pupil=" + pupil +
+               ", attributionDate=" + attributionDate +
+               ", motive='" + motive + '\'' +
+               '}';
+    }
 }
--- starter-master/src/com/uca/entity/PupilEntity.java
+++ starter-master/src/com/uca/entity/PupilEntity.java
@@ -2,4 +2,52 @@ package com.uca.entity;
 
 public class PupilEntity
 {
+    private long   id;
+    private String lastName;
+    private String firstName;
+
+    public PupilEntity()
+    {
+        // never used
+    }
+
+    long getId()
+    {
+        return id;
+    }
+
+    void setId(long id)
+    {
+        this.id = id;
+    }
+
+    String getLastName()
+    {
+        return lastName;
+    }
+
+    void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    String getFirstName()
+    {
+        return firstName;
+    }
+
+    void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "PupilEntity{" +
+               "id=" + id +
+               ", lastName='" + lastName + '\'' +
+               ", firstName='" + firstName + '\'' +
+               '}';
+    }
 }
