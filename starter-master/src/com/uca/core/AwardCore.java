f162572541cfa01df64d7a5d673c5d76b8ab636a
Alexis DRAI <drai.alexis@gmail.com>
2022-05-06 19:29:55 +0200

    Make awardCore

--- starter-master/src/com/uca/core/AwardCore.java
+++ starter-master/src/com/uca/core/AwardCore.java
@@ -1,5 +1,35 @@
 package com.uca.core;
 
+import com.uca.dao.AwardDAO;
+import com.uca.entity.AwardEntity;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class AwardCore
 {
+    public static AwardEntity create(AwardEntity obj) throws SQLException
+    {
+        return new AwardDAO().create(obj);
+    }
+
+    public static ArrayList<AwardEntity> readAll()
+    {
+        return new AwardDAO().readAll();
+    }
+
+    public static ArrayList<AwardEntity> readByStudent(long studentId)
+    {
+        return new AwardDAO().readByStudentId(studentId);
+    }
+
+    public static AwardEntity readById(long id)
+    {
+        return new AwardDAO().readById(id);
+    }
+
+    public static void deleteById(long id)
+    {
+        new AwardDAO().deleteById(id);
+    }
 }
