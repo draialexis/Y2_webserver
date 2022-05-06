2f5f2b30f2cac64b0ac9c5546bc6f44d8b61622b
Eyosias1 <eyosias16@gmail.com>
2022-05-06 17:28:10 +0200

    update the StudentCore.

--- starter-master/src/com/uca/core/StudentCore.java
+++ starter-master/src/com/uca/core/StudentCore.java
@@ -1,10 +1,15 @@
 package com.uca.core;
 
 import com.uca.dao.StudentDAO;
+import com.uca.dao.TeacherDAO;
 import com.uca.entity.StudentEntity;
+import com.uca.entity.TeacherEntity;
+
+import java.util.ArrayList;
 
 public class StudentCore
 {
+    public static ArrayList<StudentEntity> readAll() {return new StudentDAO().readAll();}
     public static StudentEntity readById(long id)
     {
         return new StudentDAO().readById(id);
