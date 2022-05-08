3943a4d62acb0258f40ec46606e067b18dde0e78
Eyosias1 <eyosias16@gmail.com>
2022-05-09 01:22:40 +0200

    get and post methods of students are set

--- starter-master/src/com/uca/StartServer.java
+++ starter-master/src/com/uca/StartServer.java
@@ -1,5 +1,7 @@
 package com.uca;
 
+import com.uca.core.StudentCore;
+import com.uca.entity.StudentEntity;
 import com.uca.login.LoginController;
 import com.uca.dao._Initializer;
 import com.uca.gui.*;
@@ -26,7 +28,7 @@ public class StartServer
         get("/teachers", (req, res) -> TeacherGUI.readAll());
 
         get("/students", (req, res) ->{
-            LoginController.ensureUserIsLoggedIn(req, res);
+            //LoginController.ensureUserIsLoggedIn(req, res);
             if (clientAcceptsHtml(req))
             {
                 return StudentGUI.readAll();
@@ -35,14 +37,69 @@ public class StartServer
         });
 
         get("/students/:id_student", (req, res) -> {
-            LoginController.ensureUserIsLoggedIn(req, res);
+            //LoginController.ensureUserIsLoggedIn(req, res);
             if (clientAcceptsHtml(req))
             {
                 return StudentGUI.readById(Long.parseLong(req.params(":id_student")));
             }
             return null;
         });
+        post("/students", (req, res) ->
+        {
+            //LoginController.ensureUserIsLoggedIn(req, res);
+            if (clientAcceptsHtml(req))
+            {
+                return StudentGUI.displayCreate();
+            }
+            return null;
+        });
 
+        post("/students/create",
+                (req, res) -> {
+                    //LoginController.ensureUserIsLoggedIn(req, res);
+                    if (clientAcceptsHtml(req))
+                    {
+                        HashMap<String, String> params = getParamFromReqBody(req.body());
+                        return StudentGUI.create(getParamUTF8(params, "lastname"),
+                                getParamUTF8(params, "firstName"));
+                    }
+                    return null;
+                });
+        post("/students/:id_student",
+                (req, res) ->{
+                    //LoginController.ensureUserIsLoggedIn(req, res);
+                    if (clientAcceptsHtml(req))
+                    {
+                        StudentGUI.displayModifPage(Long.parseLong(req.params(":id_student")));
+                        return StudentGUI.displayModifPage(Long.parseLong(req.params(":id_student")));
+                    }
+                    return null;
+                });
+        post("/students/modif/:id_student",
+                (req, res) ->{
+                    //LoginController.ensureUserIsLoggedIn(req, res);
+                    if (clientAcceptsHtml(req))
+                    {
+                        HashMap<String, String> params = getParamFromReqBody(req.body());
+                        StudentEntity student = new StudentEntity();
+                        student.setFirstName(getParamUTF8(params, "firstName"));
+                        student.setLastName(getParamUTF8(params, "lastname"));
+                        student.setId(Long.parseLong(req.params(":id_student")));
+                        //StudentGUI.update(student); // cette method est pas trea bine n'affiche pas le resultat apres modification il faut reinitialiser pour voir si sa a maercher alors on redirige
+                        StudentCore.update(student, student.getId());
+                        res.redirect("/students");
+                    }
+                    return null;
+                });
+        post("/students/delete/:id_student", (req, res) -> {
+            //LoginController.ensureUserIsLoggedIn(req, res);
+            if (clientAcceptsHtml(req))
+            {
+                StudentCore.deletbyId(Long.parseLong(req.params(":id_student")));
+                res.redirect("/students");
+            }
+            return null;
+        });
         get("/login", (req, res) -> LoginGUI.display("merci de vous identifier"));
 
         post("/login", LoginController::handleLoginPost);
