9a6259b64101f51ea581d85695dc4b46dc1024ed
Eyosias1 <eyosias16@gmail.com>
2022-05-09 01:20:56 +0200

    complete the student CORE.

--- starter-master/src/com/uca/core/StudentCore.java
+++ starter-master/src/com/uca/core/StudentCore.java
@@ -5,13 +5,18 @@ import com.uca.dao.TeacherDAO;
 import com.uca.entity.StudentEntity;
 import com.uca.entity.TeacherEntity;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class StudentCore
 {
+    public static long findLastId() throws SQLException {return new StudentDAO().findLastId();}
+    public static StudentEntity create(StudentEntity obj) {return  new StudentDAO().create(obj);}
     public static ArrayList<StudentEntity> readAll() {return new StudentDAO().readAll();}
     public static StudentEntity readById(long id)
     {
         return new StudentDAO().readById(id);
     }
+    public static StudentEntity update(StudentEntity obj, long id) {return new StudentDAO().update(obj, id);}
+    public static void deletbyId(long id_stud) {new StudentDAO().deleteById(id_stud);}
 }
