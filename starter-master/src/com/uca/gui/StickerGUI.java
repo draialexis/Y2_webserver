5e76e154f97f96d7b728e2f3d59994926e98d4ea
Eyosias1 <eyosias16@gmail.com>
2022-05-09 13:37:56 +0200

    add the the gui of Modify page and create page.

--- starter-master/src/com/uca/gui/StickerGUI.java
+++ starter-master/src/com/uca/gui/StickerGUI.java
@@ -1,11 +1,14 @@
 package com.uca.gui;
 
 import com.uca.core.StickerCore;
+import com.uca.entity.Color;
+import com.uca.entity.Description;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,4 +31,27 @@ public class StickerGUI
         input.put("sticker", StickerCore.readById(id));
         return _UtilGUI.render(template, input, new StringWriter());
     }
+    public static String dispalyCreatePage()
+            throws IOException, TemplateException{
+        Template            template = _FreeMarkerInitializer.getContext().getTemplate("stickers/createSticker.ftl");
+        Map<String, Object> input    = new HashMap<>();
+        Color color[] =  Color.values();
+        Description description[]= Description.values();
+        input.put("color", color);
+        input.put("description", description);
+        return _UtilGUI.render(template, input, new StringWriter());
+    }
+
+    public static String dispalyModifPage(long Sticker_id)
+            throws IOException, TemplateException{
+        Template            template = _FreeMarkerInitializer.getContext().getTemplate("stickers/modifieSticker.ftl");
+        Map<String, Object> input    = new HashMap<>();
+        Color color[] =  Color.values();
+        Description description[]= Description.values();
+        input.put("color", color);
+        input.put("description", description);
+        input.put("path_id", Sticker_id);
+        return _UtilGUI.render(template, input, new StringWriter());
+    }
+
 }
