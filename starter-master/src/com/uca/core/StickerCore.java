a8d5e83106be5df0be5a396a43c686ae538b4128
Eyosias1 <eyosias16@gmail.com>
2022-05-09 13:39:41 +0200

    update the sticker core class.

--- starter-master/src/com/uca/core/StickerCore.java
+++ starter-master/src/com/uca/core/StickerCore.java
@@ -3,6 +3,7 @@ package com.uca.core;
 import com.uca.dao.StickerDAO;
 import com.uca.entity.StickerEntity;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class StickerCore
@@ -16,4 +17,8 @@ public class StickerCore
     {
         return new StickerDAO().readById(id);
     }
+    public static StickerEntity create(StickerEntity obj) {return new StickerDAO().create(obj);}
+    public static long findLastId() throws SQLException {return new StickerDAO().findLastId();}
+    public static StickerEntity update(StickerEntity obj, long id) {return new StickerDAO().update(obj, id);}
+    public static void deleteById(long id) {new StickerDAO().deleteById(id);}
 }
