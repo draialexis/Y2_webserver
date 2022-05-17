4aaefc908d92dafbbe7fe8217d4d8f9e028e6c09
Alexis DRAI <drai.alexis@gmail.com>
2022-05-17 10:25:55 +0200

    Prettify and comment controller

--- starter-master/src/com/uca/StartServer.java
+++ starter-master/src/com/uca/StartServer.java
@@ -71,21 +71,25 @@ public class StartServer
         });
 
         //===============Auth & Index===============
+        // index page
         get("/", (req, res) -> IndexGUI.display());
 
+        // login page
         get("/login", (req, res) -> LoginGUI.display(null));
-
         get("/login/timeout", (req, res) -> LoginGUI.display(InfoMsg.SESSION_TERMINEE_AUTHENTIFICATION_NECESSAIRE));
-
         get("/login/redirect", (req, res) -> LoginGUI.display(InfoMsg.AUTHENTIFICATION_NECESSAIRE));
 
-        get("/hidden/signup", (req, res) -> SignUpGUI.display());
-
+        // log in / create session
         post("/login", LoginUtil::handleLoginPost);
 
+        // log out
         get("/logout", (req, res) -> LoginUtil.handleLogout(res));
 
+        // signup page
+        get("/hidden/signup", (req, res) -> SignUpGUI.display());
+
         //===============CR** teachers===============
+        // create
         post("/hidden/teachers", (req, res) -> {
             HashMap<String, String> params = getParamFromReqBody(req.body());
             return TeacherGUI.create(getParamUTF8(params, "lastname"),
@@ -95,20 +99,25 @@ public class StartServer
                                      getParamUTF8(params, "userpwd-validation"));
         });
 
+        // read all
         get("/hidden/teachers", (req, res) -> TeacherGUI.readAll());
 
+        // read one
         get("/hidden/teachers/:id_teacher",
             (req, res) -> TeacherGUI.readById(Long.parseLong(req.params(":id_teacher"))));
 
         //===============CRUD students===============
+        // create
         post("/hidden/students", (req, res) -> {
             HashMap<String, String> params = getParamFromReqBody(req.body());
             return StudentGUI.create(getParamUTF8(params, "lastname"),
                                      getParamUTF8(params, "firstname"));
         });
 
+        // read all
         get("/hidden/students", (req, res) -> StudentGUI.readAll());
 
+        // read one
         get("/hidden/students/:id_student",
             (req, res) -> StudentGUI.readById(Long.parseLong(req.params(":id_student"))));
 
@@ -120,6 +129,7 @@ public class StartServer
                                      getParamUTF8(params, "firstname"));
         });
 
+        // update
         put("/hidden/students/:id_student", (req, res) -> {
             HashMap<String, String> params = getParamFromReqBody(req.body());
             return StudentGUI.update(Long.parseLong(req.params(":id_student")),
@@ -131,18 +141,22 @@ public class StartServer
         post("/hidden/students/delete/:id_student",
              (req, res) -> StudentGUI.deleteById(Long.parseLong(req.params(":id_student"))));
 
+        // delete
         delete("/hidden/students/:id_student",
                (req, res) -> StudentGUI.deleteById(Long.parseLong(req.params(":id_student"))));
 
         //===============CRUD stickers===============
+        // create
         post("/hidden/stickers", (req, res) -> {
             HashMap<String, String> params = getParamFromReqBody(req.body());
             return StickerGUI.create(getParamUTF8(params, "color"),
                                      getParamUTF8(params, "description"));
         });
 
+        // read all
         get("/stickers", (req, res) -> StickerGUI.readAll(LoginUtil.isLoggedIn(req, res)));
 
+        // read one
         get("/stickers/:id_sticker",
             (req, res) -> StickerGUI.readById(LoginUtil.isLoggedIn(req, res),
                                               Long.parseLong(req.params(":id_sticker"))));
@@ -155,6 +169,7 @@ public class StartServer
                                      getParamUTF8(params, "description"));
         });
 
+        // update
         put("/hidden/stickers/:id_sticker", (req, res) -> {
             HashMap<String, String> params = getParamFromReqBody(req.body());
             return StickerGUI.update(Long.parseLong(req.params(":id_sticker")),
@@ -166,10 +181,12 @@ public class StartServer
         post("/hidden/stickers/delete/:id_sticker",
              (req, res) -> StickerGUI.deleteById(Long.parseLong(req.params(":id_sticker"))));
 
+        // delete
         delete("/hidden/stickers/:id_sticker",
                (req, res) -> StickerGUI.deleteById(Long.parseLong(req.params(":id_sticker"))));
 
         //===============CR*D awards===============
+        // create
         post("/hidden/awards", (req, res) -> {
             HashMap<String, String> params = getParamFromReqBody(req.body());
             return AwardGUI.create(
@@ -179,12 +196,15 @@ public class StartServer
                     Long.parseLong(getParamUTF8(params, "sticker-id")));
         });
 
+        // read all
         get("/awards", (req, res) -> AwardGUI.readAll(LoginUtil.isLoggedIn(req, res)));
 
+        // read many by student id
         get("/awards/student/:id_student",
             (req, res) -> AwardGUI.readByStudentId(LoginUtil.isLoggedIn(req, res),
                                                    Long.parseLong(req.params(":id_student"))));
 
+        // read one
         get("/awards/id/:id_award",
             (req, res) -> AwardGUI.readById(LoginUtil.isLoggedIn(req, res), Long.parseLong(req.params(":id_award"))));
 
@@ -192,6 +212,7 @@ public class StartServer
         post("/hidden/awards/delete/:id_award",
              (req, res) -> AwardGUI.deleteById(Long.parseLong(req.params(":id_award"))));
 
+        // delete
         delete("/hidden/awards/:id_award",
                (req, res) -> AwardGUI.deleteById(Long.parseLong(req.params(":id_award"))));
     }
